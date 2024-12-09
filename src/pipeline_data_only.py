6d08620d73b6a7f2b6ea14778ccb097b1b9500ef
RinTrin <tomirin.rinta@gmail.com>
2024-12-09 13:20:15 +0900

    add_city

--- src/pipeline_data_only.py
+++ src/pipeline_data_only.py
@@ -20,5 +20,26 @@ def pipeline(city_name=None):
     
 
 if __name__ == '__main__':
-    # pipeline(city_name='多摩市')
-    pipeline(city_name='厚木市')
+    import argparse
+
+    # ArgumentParserのインスタンスを作成
+    parser = argparse.ArgumentParser(description="コマンドライン引数の例")
+
+    # 引数を追加
+    parser.add_argument("input_value", help="入力値")  # 必須引数
+    parser.add_argument("--optional", help="オプション引数", default="デフォルト値")  # オプション引数
+
+    # 引数を解析
+    args = parser.parse_args()
+
+    # 引数を表示
+    print("使用する市:", args.input_value)
+    print("オプションの値:", args.optional)
+    
+    city_names = []
+    if "/" in args.input_value:
+        city_names = args.input_value.split("/")
+    # print(city_names)
+        
+    for city_name_use in city_names:
+        pipeline(city_name=str(city_name_use))
